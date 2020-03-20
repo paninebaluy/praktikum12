3839d06d88650d64a070056207c7ee9069fa96bf
Natalia Dolgushina <paninebaluy@gmail.com>
2020-03-20 13:41:24 +0300

    moved all webpack source files to public/src

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(bodyParser.json()); // for parsing data as JSON
 app.use(bodyParser.urlencoded({ extended: true })); // accepting various file types in POST requests
-app.use(express.static(path.join(__dirname, 'public'))); // for serving static files from public dir
+app.use(express.static(path.join(__dirname, 'public/dist'))); // for serving static files from public dir
 app.use(logger);
 // app uses routing described in a a separate module
 app.use('/', router);
